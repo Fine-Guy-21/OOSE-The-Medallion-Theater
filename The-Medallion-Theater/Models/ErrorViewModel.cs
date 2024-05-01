ab872a0d3f548e772edea33f502129f32529425d
Fine Guy <122565078+Fine-Guy-21@users.noreply.github.com>
2024-05-01 17:24:36 +0300

    application created

---
+++ The-Medallion-Theater/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace The_Medallion_Theater.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
